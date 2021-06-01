e0210b535976b431da22d58bb749a4b1ee48bef1
Luiz <lh.alvesf@gmail.com>
2021-06-01 11:06:07 -0300

    insert logic in controller

---
+++ app/Controllers/Http/GameController.js
@@ -0,0 +1,23 @@
+'use strict'
+
+class GameController {
+    render({ request }) {
+        /* get number from url string */
+        const guessedNumber = Number(request.input('number'))
+
+        /* if number isnt specified, inform user about it */
+        if (!guessedNumber) {
+            return 'Please specify a number passing ?number={number} to the url'
+        }
+
+        /* generate a ramdom number */
+        const randomNumber = Math.floor(Math.random() * 20) + 1
+
+        /* check if numbers match */
+        return guessedNumber === randomNumber
+        ? 'Matched'
+        : `Match failed. The random number is ${randomNumber}`
+    }
+}
+
+module.exports = GameController
--- start/routes.js
+++ start/routes.js
@@ -1,21 +1,3 @@
 const Route = use('Route')
 
-Route.get('/', ({ request }) => {
-
-  /* get number from url string */
-  const guessedNumber = Number(request.input('number'))
-
-  /* if number isnt specified, inform user about it */
-  if (!guessedNumber) {
-    return 'Please specify a number passing ?number={number} to the url'
-  }
-
-  /* generate a ramdom number */
-  const randomNumber = Math.floor(Math.random() * 20) + 1
-
-  /* check if numbers match */
-  return guessedNumber === randomNumber
-  ? 'Matched'
-  : `Match failed. The random number is ${randomNumber}`
-
-})
+Route.get('/', 'GameController.render')
