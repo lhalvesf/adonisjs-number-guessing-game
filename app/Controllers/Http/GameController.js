139b1f51be47e095d1d57247f418e3fa0d7f7e68
Luiz <lh.alvesf@gmail.com>
2021-06-01 11:13:22 -0300

    passing logic to view

--- app/Controllers/Http/GameController.js
+++ app/Controllers/Http/GameController.js
@@ -1,22 +1,15 @@
 'use strict'
 
 class GameController {
-    render({ request }) {
+    render({ request, view }) {
         /* get number from url string */
         const guessedNumber = Number(request.input('number'))
-
-        /* if number isnt specified, inform user about it */
-        if (!guessedNumber) {
-            return 'Please specify a number passing ?number={number} to the url'
-        }
 
         /* generate a ramdom number */
         const randomNumber = Math.floor(Math.random() * 20) + 1
 
-        /* check if numbers match */
-        return guessedNumber === randomNumber
-        ? 'Matched'
-        : `Match failed. The random number is ${randomNumber}`
+        /* rendering view */
+        return view.render('game', { guessedNumber, randomNumber })
     }
 }
 
